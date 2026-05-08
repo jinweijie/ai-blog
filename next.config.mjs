ab5d4289f0f0a019cae3e4fcc2afe8efbe629850
JIN Weijie <jinweijie@gmail.com>
2026-05-08 14:38:39 +0800

    feat: show deployed commit in footer

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  env: {
+    NEXT_PUBLIC_COMMIT_SHA: process.env.VERCEL_GIT_COMMIT_SHA ?? "",
+  },
 };
 
 export default nextConfig;
