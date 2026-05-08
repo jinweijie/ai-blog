3ee97af0041d075fe63d874eeace8811f13c1dbd
JIN Weijie <jinweijie@gmail.com>
2026-05-08 15:27:48 +0800

    fix: avoid replaceAll for ES2020

--- lib/markdown.ts
+++ lib/markdown.ts
@@ -16,11 +16,11 @@ function getCachedHighlighter() {
 
 function escapeHtml(value: string) {
   return value
-    .replaceAll("&", "&amp;")
-    .replaceAll("<", "&lt;")
-    .replaceAll(">", "&gt;")
-    .replaceAll("\"", "&quot;")
-    .replaceAll("'", "&#39;");
+    .replace(/&/g, "&amp;")
+    .replace(/</g, "&lt;")
+    .replace(/>/g, "&gt;")
+    .replace(/"/g, "&quot;")
+    .replace(/'/g, "&#39;");
 }
 
 function normalizeLang(info?: string) {
